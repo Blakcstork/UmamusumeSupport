75d9f00ff69e3cf29e463df4d533235f2c5d94a3
이종상 <helius96@naver.com>
2022-07-14 23:03:29 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Movie from "./routes/Movie";
 import Movies from "./routes/Movies";
+import SearchInput from "./component/searchInput";
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path = "/" element = {<Movies />} />
+          <Route path = "/" element = {<SearchInput />} />
+          <Route path = "/movies" element = {<Movies />} />
           <Route path = "/movies/:id" element = {<Movie />} />
       </Routes>
     
---
+++ src/component/searchInput.js
@@ -0,0 +1,17 @@
+
+import {useEffect, useState} from "react"
+import styles from "../css/searchInput.module.css"
+
+
+function SearchInput({searchType}){
+    return(
+        <div>
+            <input type = "text" placeholder="please type input" className={styles.search}/>
+        </div>
+    )
+}
+
+
+
+
+export default SearchInput;
