4c19afa2ac21f01fdfbbffe9a2266f30039b06e3
이종상 <helius96@naver.com>
2022-08-03 23:44:49 +0900

    there

--- src/component/RacePlanner.js
+++ src/component/RacePlanner.js
@@ -1,12 +1,32 @@
-import styles from "../css/RacePlanner.module.css"
+import {useState} from "react";
+import PlannerModal from "./PlannerModal.js";
+
+import styles from "../css/RacePlanner.module.css";
+import dummy from "../tempServer/race.json";
 
 
-function RacePlanner(){
+
+function LittleRaceList({img, name, }){
+
+}
+
+
+function RacePlanner({season}){
+    const [popup, setPopup] = useState({open : false, title : "", message: "", callback : false});
+
+    const onClick = (e, month, day) => {
+        setPopup({open: true, title : `It's race`, message : "Hello!"})
+        console.log(`${month}, ${day}`)
+    }
+
 
     return (
         <div>
-            <div>주니어</div>
+            <PlannerModal open = {popup.open} setPopup = {setPopup} message = {popup.message} title = {popup.title} callback = {popup.callback}/>
             <table  className={styles.planTable}>
+                <thead>
+                    주니어
+                </thead>
                 <tbody>
                     <tr className={styles.pNames}>
                         <td>1월 전</td>
@@ -17,12 +37,12 @@ function RacePlanner(){
                         <td>3월 후</td>
                     </tr>
                     <tr className= {styles.pContents}>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> +</td>
-                        <td> + </td>
-                        <td> + </td>
+                        <td onClick = {(e) => onClick(e,1,"전")}> <img src = "/images/icons/plus.png" alt="10" /></td>
+                        <td onClick = {(e) => onClick(e,1,"후")}> <img src = "/images/icons/plus.png" alt="11"/> </td>
+                        <td onClick = {(e) => onClick(e,2,"전")}> <img src = "/images/icons/plus.png" alt="20"/> </td>
+                        <td onClick = {(e) => onClick(e,2,"후")}> <img src = "/images/icons/plus.png" alt="21"/> </td>
+                        <td onClick = {(e) => onClick(e,3,"전")}> <img src = "/images/icons/plus.png" alt="30"/> </td>
+                        <td onClick = {(e) => onClick(e,3,"후")}> <img src = "/images/icons/plus.png" alt="31"/> </td>
                     </tr>
                     <tr className={styles.pNames}>
                         <td>4월 전</td>
@@ -33,12 +53,12 @@ function RacePlanner(){
                         <td>6월 후</td>
                     </tr>
                     <tr className= {styles.pContents}>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
+                        <td onClick = {(e) => onClick(e,4,"전")}> <img src = "/images/icons/plus.png" alt="40"/></td>
+                        <td onClick = {(e) => onClick(e,4,"후")}> <img src = "/images/icons/plus.png" alt="41"/> </td>
+                        <td onClick = {(e) => onClick(e,5,"전")}> <img src = "/images/icons/plus.png" alt="50"/> </td>
+                        <td onClick = {(e) => onClick(e,5,"후")}> <img src = "/images/icons/plus.png" alt="51"/> </td>
+                        <td onClick = {(e) => onClick(e,6,"전")}> <img src = "/images/icons/plus.png" alt="60"/> </td>
+                        <td onClick = {(e) => onClick(e,6,"후")}> <img src = "/images/icons/plus.png" alt="61"/> </td>
                     </tr>
                     <tr className={styles.pNames}> 
                         <td>7월 전</td>
@@ -49,12 +69,12 @@ function RacePlanner(){
                         <td>9월 후</td>
                     </tr>
                     <tr className= {styles.pContents}>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
+                        <td onClick = {(e) => onClick(e,7,"전")}> <img src = "/images/icons/plus.png" alt="70"/></td>
+                        <td onClick = {(e) => onClick(e,7,"후")}> <img src = "/images/icons/plus.png" alt="71"/> </td>
+                        <td onClick = {(e) => onClick(e,8,"전")}> <img src = "/images/icons/plus.png" alt="80"/> </td>
+                        <td onClick = {(e) => onClick(e,8,"후")}> <img src = "/images/icons/plus.png" alt="81"/> </td>
+                        <td onClick = {(e) => onClick(e,9,"전")}> <img src = "/images/icons/plus.png" alt="90"/> </td>
+                        <td onClick = {(e) => onClick(e,9,"후")}> <img src = "/images/icons/plus.png" alt="91"/> </td>
                     </tr>
                     <tr className={styles.pNames}>
                         <td>10월 전</td>
@@ -65,15 +85,14 @@ function RacePlanner(){
                         <td>12월 후</td>
                     </tr>
                     <tr className= {styles.pContents}>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
-                        <td> + </td>
+                        <td onClick = {(e) => onClick(e,10,"전")}> <img src = "/images/icons/plus.png" alt="100"/></td>
+                        <td onClick = {(e) => onClick(e,10,"후")}> <img src = "/images/icons/plus.png" alt="101"/> </td>
+                        <td onClick = {(e) => onClick(e,11,"전")}> <img src = "/images/icons/plus.png" alt="110"/> </td>
+                        <td onClick = {(e) => onClick(e,11,"후")}> <img src = "/images/icons/plus.png" alt="111"/> </td>
+                        <td onClick = {(e) => onClick(e,12,"전")}> <img src = "/images/icons/plus.png" alt="120"/> </td>
+                        <td onClick = {(e) => onClick(e,12,"후")}> <img src = "/images/icons/plus.png" alt="121"/> </td>
                     </tr>
                 </tbody>
-
             </table>
         </div>
     )
