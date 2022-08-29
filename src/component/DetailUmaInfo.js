3af2b89a1a71982377898045677887f27c96eb68
이종상 <helius96@naver.com>
2022-08-29 23:40:04 +0900

    hard time

--- src/component/DetailUmaInfo.js
+++ src/component/DetailUmaInfo.js
@@ -3,8 +3,39 @@ import Accordion from "react-bootstrap/Accordion"
 import styles from "../css/DetailUmaInfo.module.css"
 
 
-function DetailUmaInfo({}){
+function DetailUmaInfo({nickname, name, rarity, img, trackType, distanceType, runType, stat, uniqueSkill, ownSkill, grown, awakenSkill, goal}){
 
+    return(
+        <div>
+            <div>
+
+            </div>
+            <div>
+                <table>
+                    <thead>
+
+                    </thead>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+            <Accordion.Item eventKey="0">
+            <Accordion.Header>GOAL Here</Accordion.Header>
+                <Accordion.Body>
+                    <ul>
+                    {goal.map((g) => 
+                    (                        <li>
+                            {g.name}
+                        </li>)
+                    )}
+                    </ul>
+
+                </Accordion.Body>
+          </Accordion.Item>
+            </div>
+        </div>
+    )
 }
 
 
