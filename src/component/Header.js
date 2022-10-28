eaed3f8f531618c3f98c3a7a62769829acd36a29
이종상 <helius96@naver.com>
2022-10-28 22:08:28 +0900

    interviewed

--- src/component/Header.js
+++ src/component/Header.js
@@ -5,11 +5,19 @@ import {Link} from "react-router-dom";
 function Header(){
     return (
         <div className={styles.header}>
-            <div>
-                <Link to = {`/`}> <h1 className={styles.headerText}>우마무스메 육성 도우미</h1> </Link>
-                <p>도-모. 여기는 헤더입니다.</p>
+            <div className = {styles.header_header}>
+                <div>
+                    <Link to = {`/`}> <h1 className={styles.headerText}>우마무스메 육성 도우미</h1> </Link>
+                    <p>도-모. 여기는 헤더입니다.</p>
+                </div>
+                <div>
+                    여기에 홈 마크
+                </div>
+                <div>
+                    여기에 깃허브 링크
+                </div>
             </div>
-            <div>
+            <div className = {styles.nav}>
                 <InfoNavigator />
             </div>
         </div>
